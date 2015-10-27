4f76c5f669d6f1b4c368ac7ae3185f8806ad0c4c
JoseNavarroOrtiz <ortiznavarrojose@live.com>
2015-10-27 08:31:16 +0100

    Actualizado

--- Primeros ejercicios/Tema5/15/escribePotencia.java
+++ Primeros ejercicios/Tema5/15/escribePotencia.java
@@ -4,14 +4,19 @@
  *  Jose Navarro Ortiz
  */
  
-public class escribePotencia {be
+public class escribePotencia {
   public static void main(String[] args) {
   
       System.out.println("Introduce la base y el exponente y se mostrara por pantalla la sucesion de potencias");
       System.out.println("Introduce la base");
-      float base = Float.parsefloat(System.console().readLine());
+      int base = Integer.parseInt(System.console().readLine());
       System.out.println("Introduce el exponente");
       int exponente = Integer.parseInt(System.console().readLine());
-  
+      int contador =1;
+      while(contador<=exponente){
+          System.out.println(base +"|" + contador+ "|");
+          contador++;
+        
+      }
     }
   }
